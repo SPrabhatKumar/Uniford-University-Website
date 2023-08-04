1c62839c436eb031db1da999fd98ea2fb1fe7004
S.Prabhat Kumar <112465806+SPrabhatKumar@users.noreply.github.com>
2023-08-04 21:35:41 +0530

    Add files via upload
    Initial Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// javascript for toggle menu
+const navLinks = document.getElementById("navLinks");
+function showMenu() {
+    navLinks.style.right = "0"
+    // navLinks.style.display="inline-block";
+}
+function hideMenu() {
+    navLinks.style.right = "-200px"
+    // navLinks.style.display="none";
+
+}
